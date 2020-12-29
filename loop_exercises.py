537f0167190f1ece8b365bfd72cc2b5dbb95de04
JocelynJeriah <jocelyn.jeriah@gmail.com>
2020-12-29 16:13:15 -0500

    adding progress to loop exercises

--- loop_exercises.py
+++ loop_exercises.py
@@ -85,7 +85,7 @@ print(divisible_by_ten([20, 25, 30, 35, 40]))
 def add_greetings(names):
   greetings = []
   for name in names:
-    greetings.append(f"Hello, {name}")
+    greetings.append("Hello, " + name)
   return greetings
 
 print(add_greetings(["Owen", "Max", "Sophie"]))
@@ -117,3 +117,107 @@ print(delete_starting_evens([4, 5, 13]))
 # The function should create a new empty list and add every element from lst that has an odd index. The function should then return this new list.
 
 # For example, odd_indices([4, 3, 7, 10, 11, -2]) should return the list [3, 10, -2].
+#One way to do it
+# def odd_indices(lst):
+#     new_lst = []
+#     for i in range(len(lst)):
+#         if i % 2 != 0:
+#             new_lst.append(lst[i])
+#     return new_lst
+
+#Second way to do it
+# def odd_indices(lst):
+#     new_lst = []
+#     for index in range(1, len(lst), 2):
+#         new_lst.append(lst[index])
+#     return new_lst
+
+#Third way to do it
+def odd_indices(lst):
+    new_lst = [lst[index] for index in range(1, len(lst), 2)]
+    return new_lst
+
+print(odd_indices([4, 3, 7, 10, 11, -2]))
+
+#Exponents
+# Create a function named exponents() that takes two lists as parameters named bases and powers. Return a new list containing every number in bases raised to every number in powers.
+
+# For example, consider the following code.
+
+# exponents([2, 3, 4], [1, 2, 3])
+# the result would be the list [2, 4, 8, 3, 9, 27, 4, 16, 64]. It would first add two to the first. Then two to the second. Then two to the third, and so on.
+
+#One way to do it
+# def exponents(bases, powers):
+#   exponential = []
+#   for base in bases:
+#     for power in powers:
+#       exponential.append(base ** power)
+#   return exponential
+
+#Second way to do it
+def exponents(bases, powers):
+    exponential = [ base ** power for base in bases for power in powers]
+    return exponential
+
+print(exponents([2, 3, 4], [1, 2, 3]))
+
+#Larger Sum
+# Create a function named larger_sum() that takes two lists of numbers as parameters named lst1 and lst2.
+
+# The function should return the list whose elements sum to the greater number. If the sum of the elements of each list are equal, return lst1.
+
+def larger_sum(lst1, lst2):
+    sum1 = 0
+    sum2 = 0
+    for x in lst1:
+        sum1 += x
+    for y in lst2:
+        sum2 += y
+    if sum1 > sum2:
+        return lst1
+    elif sum1== sum2:
+        return lst1
+    else:
+        return lst2
+
+print(larger_sum([1, 9, 5], [2, 3, 7]))
+
+# Over 9000
+
+# Create a function named over_nine_thousand() that takes a list of numbers named lst as a parameter.
+
+# The function should sum the elements of the list until the sum is greater than 9000. When this happens, the function should return the sum. If the sum of all of the elements is never greater than 9000, the function should return total sum of all the elements. If the list is empty, the function should return 0.
+
+# For example, if lst was [8000, 900, 120, 5000], then the function should return 9020.
+
+def over_nine_thousand(lst):
+    sum = 0
+    for i in range(len(lst)):
+        sum += lst[i]   
+        if sum > 9000:
+            return sum
+    return sum
+
+print(over_nine_thousand([8000, 900, 120, 5000]))
+
+#Max Num
+# Create a function named max_num() that takes a list of numbers named nums as a parameter.
+
+# The function should return the largest number in nums
+
+def max_num(nums):
+    max = nums[0]
+    for i in range(len(nums)):
+        if nums[i] > max:
+            max = nums[i]
+    return max
+
+print(max_num([50, -10, 0, 75, 20]))
+
+# Same Values
+# Write a function named same_values() that takes two lists of numbers of equal size as parameters.
+
+# The function should return a list of the indices where the values were equal in lst1 and lst2.
+
+# For example, the following code should return [0, 2, 3]
