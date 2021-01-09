53d3024c8c1300510b862701eaae23fe2415fa83
JocelynJeriah <jocelyn.jeriah@gmail.com>
2021-01-09 13:14:06 -0500

    adding all remaining exercises and solutions

--- strings/string_methods.py
+++ strings/string_methods.py
@@ -81,3 +81,93 @@ print(x_length_words("i like apples", 2))
 # should print False
 print(x_length_words("he likes apples", 2))
 # should print True
+
+# Check name
+# Write a function called check_for_name that takes two strings as parameters named sentence and name. The function should return True if name appears in sentence in all lowercase letters, all uppercase letters, or with any mix of uppercase and lowercase letters. The function should return False otherwise.
+
+# For example, the following three calls should all return True:
+
+# check_for_name("My name is Jamie", "Jamie")
+# check_for_name("My name is jamie", "Jamie")
+# check_for_name("My name is JAMIE", "Jamie")
+
+def check_for_name(sentence, name):
+  if name.lower() in sentence.lower():
+    return True
+  else:
+    return False
+
+print(check_for_name("My name is Jamie", "Jamie"))
+# should print True
+print(check_for_name("My name is jamie", "Jamie"))
+# should print True
+print(check_for_name("My name is Samantha", "Jamie"))
+# should print False
+
+#Every other letter
+# Create a function named every_other_letter that takes a string named word as a parameter. The function should return a string containing every other letter in word.
+
+def every_other_letter(word):
+  new_word = ""
+  for i in range(0, len(word), 2):
+    new_word += word[i]
+    print(word[i])
+  return new_word
+
+# Uncomment these function calls to test your function:
+print(every_other_letter("Codecademy"))
+# should print Cdcdm
+print(every_other_letter("Hello world!"))
+# should print Hlowrd
+print(every_other_letter(""))
+# should print 
+
+# Reverse
+# Write a function named reverse_string that has a string named word as a parameter. The function should return word in reverse.
+
+def reverse_string(word):
+  reversed_word = ""
+  for i in range(len(word) - 1, -1, -1):
+    reversed_word += word[i]
+  return reversed_word
+
+print(reverse_string("Codecademy"))
+# should print ymedacedoC
+print(reverse_string("Hello world!"))
+# should print !dlrow olleH
+print(reverse_string(""))
+# should print
+
+# Make Spoonerism
+# A Spoonerism is an error in speech when the first syllables of two words are switched. For example, a Spoonerism is made when someone says "Belly Jeans" instead of "Jelly Beans".
+
+# Write a function called make_spoonerism that takes two strings as parameters named word1 and word2. Finding the first syllable of a word is a difficult task, so for our function, we're going to switch the first letters of each word. Return the two new words as a single string separated by a space.
+
+def make_spoonerism(word1, word2):
+  new_word1 = word2[0]  + word1[1:]
+  new_word2 = word1[0] + word2[1:] 
+  return "{} {}".format(new_word1, new_word2)
+
+# Uncomment these function calls to test your function:
+print(make_spoonerism("Codecademy", "Learn"))
+# should print Lodecademy Cearn
+print(make_spoonerism("Hello", "world!"))
+# should print wello Horld!
+print(make_spoonerism("a", "b"))
+# should print b a
+
+# Add Exclamation
+# Create a function named add_exclamation that has one parameter named word. This function should add exclamation points to the end of word until word is 20 characters long. If word is already at least 20 characters long, just return word.
+
+def add_exclamation(word):
+  word_length = len(word)
+  if word_length < 20:
+    exclamation = 20 - word_length
+    return word + ("!" * exclamation)
+  else:
+    return word
+
+print(add_exclamation("Codecademy"))
+# should print Codecademy!!!!!!!!!!
+print(add_exclamation("Codecademy is the best place to learn"))
+# should print Codecademy is the best place to learn
